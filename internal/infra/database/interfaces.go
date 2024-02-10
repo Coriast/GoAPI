8482deb2979f31e649d51a27bd4b78e854b80e18
Coriast <pedrowillianpesquisador@gmail.com>
2024-02-10 20:57:17 -0300

    Testes de Produto

---
+++ internal/infra/database/interfaces.go
@@ -0,0 +1,16 @@
+package database
+
+import "GoAPI/internal/entity"
+
+type UserI interface {
+	Create(user *entity.User) error
+	FindByEmail(email string) (*entity.User, error)
+}
+
+type ProductI interface {
+	Create(product *entity.Product) error
+	FindAll(page, limit int, sort string) ([]entity.Product, error)
+	FindByID(id string) (*entity.Product, error)
+	Update(product *entity.Product) error
+	Delete(id string) error
+}
---
+++ test/infra/database/UserDB_test.go
@@ -0,0 +1,49 @@
+package database
+
+import (
+	"GoAPI/internal/entity"
+	"GoAPI/internal/infra/database"
+	"github.com/stretchr/testify/assert"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+	"testing"
+)
+
+func TestCreateUser(t *testing.T) {
+	db, err := gorm.Open(sqlite.Open("file::memory:"), &gorm.Config{})
+	if err != nil {
+		t.Error(err)
+	}
+	db.AutoMigrate(&entity.User{})
+	user, _ := entity.NewUser("Joao", "j@j.com", "123456")
+	userDB := database.NewUser(db)
+
+	err = userDB.Create(user)
+	assert.Nil(t, err)
+
+	var userFound entity.User
+	err = db.First(&userFound, "id = ?", user.ID).Error
+	assert.Nil(t, err)
+	assert.Equal(t, user.ID, userFound.ID)
+	assert.Equal(t, user.Name, userFound.Name)
+	assert.Equal(t, user.Email, userFound.Email)
+}
+
+func TestFindByEmail(t *testing.T) {
+	db, err := gorm.Open(sqlite.Open("file::memory:"), &gorm.Config{})
+	if err != nil {
+		t.Error(err)
+	}
+	db.AutoMigrate(&entity.User{})
+	user, _ := entity.NewUser("Joao", "j@j.com", "123456")
+	userDB := database.NewUser(db)
+
+	err = userDB.Create(user)
+	assert.Nil(t, err)
+
+	userFound, err := userDB.FindByEmail(user.Email)
+	assert.Nil(t, err)
+	assert.Equal(t, user.ID, userFound.ID)
+	assert.Equal(t, user.Name, userFound.Name)
+	assert.Equal(t, user.Email, userFound.Email)
+}
