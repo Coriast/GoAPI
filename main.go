4e690b5e863b13820edc1c4a898242d83b7a7fd5
Coriast <pedrowillianpesquisador@gmail.com>
2024-02-14 14:29:49 -0300

    finalização do projeto

--- main.go
+++ main.go
@@ -2,16 +2,36 @@ package main
 
 import (
 	"GoAPI/configs"
+	_ "GoAPI/docs"
 	"GoAPI/internal/entity"
 	"GoAPI/internal/infra/database"
 	"GoAPI/internal/infra/webserver/handlers"
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/jwtauth"
+	httpSwagger "github.com/swaggo/http-swagger"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 	"net/http"
 )
 
+// @title Go API
+// @version 1.0
+// @description Product API
+// @termsOfService http://swagger.io/terms/
+
+// @contact.name Pedro William
+// @contact.url http://www.swagger.io/support
+// @contact.email support@swagger.io
+
+// @license.name Apache 2.0
+// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
+
+// @host localhost:8000
+// @BasePath /
+// @securityDefinitions.apikey ApiKeyAuth
+// @in header
+// @name Authorization
 func main() {
 	configs, err := configs.LoadConfig(".")
 	if err != nil {
@@ -30,14 +50,19 @@ func main() {
 
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
-	r.Post("/products", productH.CreateProduct)
-	r.Get("/products/{id}", productH.GetProduct)
-	r.Get("/products", productH.GetProducts)
-	r.Put("/products/{id}", productH.UpdateProduct)
-	r.Delete("/products/{id}", productH.DeleteProduct)
-
+	r.Route("/products", func(r chi.Router) {
+		r.Use(jwtauth.Verifier(configs.TokenAuth))
+		r.Use(jwtauth.Authenticator)
+		r.Post("/", productH.CreateProduct)
+		r.Get("/{id}", productH.GetProduct)
+		r.Get("/", productH.GetProducts)
+		r.Put("/{id}", productH.UpdateProduct)
+		r.Delete("/{id}", productH.DeleteProduct)
+	})
 	r.Post("/users", userH.CreateUser)
 	r.Post("/users/generateToken", userH.GetJWT)
+
+	r.Get("/docs/*", httpSwagger.Handler(httpSwagger.URL("http://localhost:8000/docs/doc.json")))
 
 	http.ListenAndServe(":8000", r)
 }
